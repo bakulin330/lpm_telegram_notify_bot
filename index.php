075a20cc0e060d616c796cca13ead31349bdd233
Artyom Maslov <arte.mas@mail.ru>
2016-01-12 09:48:00 +0400

    index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $token = '156771533:AAFtGPT_o3MFuPRBnuYwOZGfNHWt_FivTy4';
 $site = 'https://api.telegram.org/bot'.$token;
 
-$update = file_get_contents($site."/getupdates");
+$update = file_get_contents($site."/sendmessage");
 
 print_r($update);
 
