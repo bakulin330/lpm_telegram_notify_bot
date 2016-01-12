4c146097903d6c6bba1d4973e89619e36b9ab093
Artyom Maslov <arte.mas@mail.ru>
2016-01-12 11:24:09 +0400

    index.php

--- index.php
+++ index.php
@@ -12,4 +12,9 @@ curl_setopt($curl, CURLOPT_URL, $site."/getupdates");
 $result = curl_exec($curl);
 curl_close($curl);
 
+$update = file_get_contents($site."/getupdates");
+
+print_r($update);
+
+
 ?>
