ca348739df536c71d60e759cf388955723d28d3c
Artyom Maslov <arte.mas@mail.ru>
2016-01-12 13:05:27 +0400

    telegram class; webhook setter

---
+++ webhook.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 12.01.2016
+ * Time: 12:01
+ */
+
+$cert_file = '/etc/nginx/keys/tele.pem';
+
+if (!isset($argv[1])){
+    die('set first param to "on" or "off"');
+}
+
+require dirname(__FILE__).DIRECTORY_SEPARATOR.'Telegram.php';
+
+$telegram = new Telegram('156771533:AAFtGPT_o3MFuPRBnuYwOZGfNHWt_FivTy4', 'wp.12qw.ru/telegram/index.php');
+echo $telegram->setWebhook('on'===$argv[1], $cert_file);
