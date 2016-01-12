934e549f209d55da08bae89975bd8cf25ccc0636
Artyom Maslov <arte.mas@mail.ru>
2016-01-12 13:13:53 +0400

    log request

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
-$token = '156771533:AAFtGPT_o3MFuPRBnuYwOZGfNHWt_FivTy4';
-$site = 'https://api.telegram.org/bot'.$token;
 
-$update = file_get_contents($site."/getupdates");
+file_put_contents('log.txt', date('H:i:s')."\n", FILE_APPEND);
 
-print_r($update);
-
-
-?>
+//$token = '156771533:AAFtGPT_o3MFuPRBnuYwOZGfNHWt_FivTy4';
+//$site = 'https://api.telegram.org/bot'.$token;
+//
+//$update = file_get_contents($site."/getupdates");
+//
+//print_r($update);
