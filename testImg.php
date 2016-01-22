e6d3e35145b15e9b233037302ecb4e9960195a2e
Artyom Maslov <arte.mas@mail.ru>
2016-01-22 14:43:35 +0400

    z

--- testImg.php
+++ testImg.php
@@ -7,27 +7,12 @@ $message = [
     'to' => 'test <arte.mas@mail.ru>',
     'subject' => 'test',
     'text' => 'test',
-    'html' => '<html> Image: <img src="cid:png.png"> </html>',
+    'html' => '<html> Image: <img src="cid:png.png"> <img src="cid:unnamed.png"> </html>',
     'o:tracking' => 'yes',
     'o:tracking-clicks' => 'yes',
     'o:tracking-opens' => 'yes',
-    'inline' => '@./png.png',
+    'inline' => ['@./png.png', '@./unnamed.png']
 ];
-
-
-
-//'inline' => array('@./test.jpeg')
-//'inline' => array('./test.jpeg')
-//'inline' => array('/test.jpeg')
-//'inline' => array('test.jpeg')
-//'inline' => array('@test.jpeg')
-//
-//['inline' => array('@./test.jpeg')]
-//['inline' => array('./test.jpeg')]
-//['inline' => array('/test.jpeg')]
-//['inline' => array('test.jpeg')]
-//['inline' => array('@test.jpeg')]
-
 
 function send($message)
 {
@@ -46,7 +31,6 @@ function send($message)
     curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
     curl_setopt($ch, CURLOPT_URL,
     'https://api.mailgun.net/v3/sandbox6ab3b5b2e70c4cec9b2749264474a34b.mailgun.org/messages'
-//        'http://wp.12qw.ru/telegram/sss.php'
     );
 //    curl_setopt($ch, CURLOPT_INFILESIZE, filesize(DIR_ROOT.'test.jpeg'));
     curl_setopt($ch, CURLOPT_POSTFIELDS, $message);
@@ -54,15 +38,7 @@ function send($message)
         "Content-Type: multipart/form-data",
     ]);
     curl_setopt($ch, CURLOPT_HEADER, false);
-//    curl_setopt($ch, CURLOPT_HTTPHEADER, [
-//        "Content-Type: multipart/form-data",
-//    ]);
     $result = curl_exec($ch);
-//if (false === $result) {
-//    $errno = curl_errno($ch);
-//    $error = curl_error($ch);
-//    $this->logError("Curl returned error $errno: $error\n");
-//}
     curl_close($ch);
     return $result;
 
