2240b08ef8bb4151a04f714713eaad601a9efe80
Artyom Maslov <arte.mas@mail.ru>
2016-01-28 18:27:13 +0400

    z

--- WebsiteScreenshot/classes/WebShot.php
+++ WebsiteScreenshot/classes/WebShot.php
@@ -27,7 +27,7 @@ class WebShot
 //        }
 
         //$command = "phantomjs ".DIR_SCRIPT."getScreenshot.js $this->url ".DIR_TMP.$this->file_name.".png $this->browser_width $this->browser_height";
-        $command = "phantomjs ".DIR_SCRPIT."getScreenshot.js $this->url ".DIR_TMP.$this->file_name.".png $this->browser_width $this->browser_height";
+        $command = "phantomjs ".DIR_SCRIPT."getScreenshot.js $this->url ".DIR_TMP.$this->file_name.".png $this->browser_width $this->browser_height";
         shell_exec($command);
         $link = DIR_TMP.$this->file_name.".png";
         //echo 'cmd:'.$command."<br/>ret:".print_r($ret,true)."<br/>out:".print_r($out,true);
