03b35fa451197e8e035ca3e9337d33ecf777ba70
Artyom Maslov <arte.mas@mail.ru>
2016-02-01 17:53:29 +0400

    vk bot

--- VkBot/classes/Bot.php
+++ VkBot/classes/Bot.php
@@ -14,7 +14,7 @@ class Bot
     {
         $this->db = new DatabaseImitation();
         $this->url = 'https://api.vk.com/method/';
-        $this->access_token = "9f9a7a16f4b31d712d1df6d7e9b0de63f7fa102b794b4be8ab3c8d61962b0988e05b8f5e7b741019a0257";
+        $this->access_token = "c1510f8efb0cbe4793684257e3855a3ebf55da8a7b4b3b62dd2bc6211d89131250d5c223be6f0cdac5f8b";
         $this->bot_id = 16309784;
     }
 
