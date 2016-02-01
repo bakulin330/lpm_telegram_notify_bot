8722522c95df881f8f34488379bc6d71f196b0f4
Artyom Maslov <arte.mas@mail.ru>
2016-02-01 18:45:57 +0400

    vk bot

---
+++ VkBot/code.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 01.02.2016
+ * Time: 17:45
+ */
+?>
+
+<a href="https://oauth.vk.com/authorize?client_id=5256044&display=page&redirect_uri=http://<?=$_SERVER['HTTP_HOST'];?>&scope=friends,messages,offline&response_type=code&v=5.44">code</a>
