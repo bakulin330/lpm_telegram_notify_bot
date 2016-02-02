d38b01a7f12f093b9b35ab051fe8c5d975f114f3
Artyom Maslov <arte.mas@mail.ru>
2016-02-02 09:35:33 +0400

    vk bot

--- VkBot/code.php
+++ VkBot/code.php
@@ -10,4 +10,4 @@
 
 <a href="https://oauth.vk.com/authorize?client_id=5256044&display=page&redirect_uri=http://<?=$_SERVER['HTTP_HOST'];?>/telegram/VkBot/code.php&scope=friends,messages,offline&response_type=code&v=5.44">code</a>
 <br>
-<a href="https://oauth.vk.com/authorize?client_id=5267111&display=page&redirect_uri=redirect_uri=http://wp.12qw.ru/telegram/VkBot/index.php&scope=friends,offline,messages&response_type=token&v=5.44"> code 2</a>
+<a href="https://oauth.vk.com/authorize?client_id=5267111&display=page&redirect_uri=redirect_uri=redirect_uri=https://oauth.vk.com/blank.html&scope=friends,offline,messages&response_type=token&v=5.44"> code 2</a>
