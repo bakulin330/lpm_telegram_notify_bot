288dfc101a8a82ac7a955ac4cc32c6c008ced7b2
Artyom Maslov <arte.mas@mail.ru>
2016-02-02 10:21:03 +0400

    vk bot

--- VkBot/classes/Bot.php
+++ VkBot/classes/Bot.php
@@ -29,7 +29,7 @@ class Bot
 
         $params += $par;
 
-        $q = "https://api.vk.com/method/messages.getDialogs?access_token=359dcf966f4651ed41f9520550e05bc23e8b8ad598aec2dd1eeaaf482bb18ab868ddb21b553837c15ef41&v=5.44&count=1&unread=1&preview_length=10";
+        $q = "https://api.vk.com/method/messages.getHistory?access_token=359dcf966f4651ed41f9520550e05bc23e8b8ad598aec2dd1eeaaf482bb18ab868ddb21b553837c15ef41&v=5.44&count=4&user_id=16309784";
 
 //        echo 'params: '.vd( $params ).'<br/>';
 //        echo 'params: '.vd( http_build_query($params) ).'<br/>';
