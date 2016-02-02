f4af83fa00e5cfc8f8c973f12d6377b006e42b68
Artyom Maslov <arte.mas@mail.ru>
2016-02-02 09:50:57 +0400

    vk bot

--- VkBot/code.php
+++ VkBot/code.php
@@ -12,4 +12,4 @@
 <br>
 <a href="https://oauth.vk.com/authorize?client_id=5267111&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends,offline,messages&response_type=token&v=5.44&revoke=1"> code 2</a>
 <br>
-<a href="https://api.vk.com/method/messages.getHistory?count=4&user_id=16309784&access_token=359dcf966f4651ed41f9520550e05bc23e8b8ad598aec2dd1eeaaf482bb18ab868ddb21b553837c15ef41"> refer</a>
+<a href="https://api.vk.com/method/messages.getHistory?count=4&user_id=16309784&access_token=359dcf966f4651ed41f9520550e05bc23e8b8ad598aec2dd1eeaaf482bb18ab868ddb21b553837c15ef41&v=5.44"> refer</a>
