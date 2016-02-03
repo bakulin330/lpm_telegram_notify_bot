d75d1feed237d493822e9740efe3fab6aee2b20d
Artyom Maslov <arte.mas@mail.ru>
2016-02-03 12:22:57 +0400

    vk bot

--- VkBot/curlTest.php
+++ VkBot/curlTest.php
@@ -18,22 +18,26 @@ $result = file_get_contents($url,false, stream_context_create(array(
         ))));
 preg_match("#https:\\\/\\\/m\.vk\.com\\\/login\?act=security_check&api_hash=(\w*)#",$result,$match);
 //        var_dump($match);
-$hash  = $match[0][1];
+$hash  = $match[1];
 //$ch = curl_init("https://m.vk.com/login?act=security_check&api_hash=$hash");
-$ch = curl_init("https://m.vk.com/login?");
+$url = "https://m.vk.com/login?act=security_check&api_hash=$hash";
+$ch = curl_init($url);
 curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1)");
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_VERBOSE, 1);
+//curl_setopt($ch, CURLOPT_VERBOSE, 1);
 curl_setopt($ch, CURLOPT_HEADER, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, "act=security_check&api_hash=$hash");
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-    'content-type: application/x-www-form-urlencoded',
-));
+//curl_setopt($ch, CURLOPT_POSTFIELDS, "act=security_check&api_hash=$hash");
+//curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+//    'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+//    'content-type: application/x-www-form-urlencoded',
+//));
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
 
 $content = curl_exec($ch);
 curl_close($ch);
-var_dump($content);
+htmlspecialchars($content);
+
+preg_match("#login\.php\?act=security_check&to=&hash=(\w*)&api_hash=(\w*)#",$content,$hashes);
+var_dump($hashes);
